e027824f9eaa624c125191c000f15e5b01603910
terratakashi <terratakashi@gmail.com>
2013-10-16 19:27:20 +0800

    only check password presence when sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   validates :full_name, :presence => true
   validates :email, :presence => true
   validates :email, :uniqueness => true
+  validates :password, :presence => true, on: :create
+  validates :password_confirmation, :presence => true, on: :create
 end
