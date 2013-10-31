a797a2faead42bc3985bf85e116ed1b598698072
terratakashi <terratakashi@gmail.com>
2013-10-31 14:52:23 +0800

    push normalize_queue_items into mode

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -21,14 +21,14 @@ class QueueItemsController < ApplicationController
   def destroy
     queue_item = QueueItem.find(params[:id])
     queue_item.destroy if queue_item.user == current_user
-    normalize_queue_item_position
+    current_user.normalize_queue_item_position
     redirect_to my_queue_path
   end
 
   def update_queue
     begin
       update_queue_items
-      normalize_queue_item_position
+      current_user.normalize_queue_item_position
     rescue ActiveRecord::RecordInvalid
       flash[:error] = "Invalid position number!"
     end
@@ -53,12 +53,6 @@ class QueueItemsController < ApplicationController
           queue_item.update_attributes!(position: queue_item_data[:position])
         end
       end
-    end
-  end
-
-  def normalize_queue_item_position
-    current_user.queue_items.each_with_index do |queue_item, index|
-      queue_item.update_attributes(position: index + 1)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,11 @@ class User < ActiveRecord::Base
   validates :full_name, :presence => true
   validates :email, :presence => true, :uniqueness => true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/}
   validates :password, :presence => true, on: :create
+
+
+  def normalize_queue_item_position
+    queue_items.each_with_index do |queue_item, index|
+      queue_item.update_attributes(position: index + 1)
+    end
+  end
 end
