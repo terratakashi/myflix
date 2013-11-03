4e93d055b2f81bcdfe4dbcd933b0ba698e6b2ab5
terratakashi <terratakashi@gmail.com>
2013-11-04 05:33:29 +0800

    user interacts with the queue spec completed

--- spec/features/user_interacts_with_queue_spec.rb
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -8,22 +8,51 @@ feature "User interacts with the queue" do
     video3 = Fabricate(:video, categories: categories)
 
     user_sign_in
-    find("a[href='#{video_path(video1)}']").click
-    expect(page).to have_content(video1.title)
 
-    click_link("+ My Queue")
-    expect(page).to have_content(video1.title)
+    add_video_to_queue(video1)
+    expect_video_to_be_in_queue(video1)
 
-    click_link(video1.title)
-    expect(page).not_to have_content("+ My Queue")
+    visit video_path(video1)
+    expect_link_not_to_be_seen("+ My Queue")
 
+    add_video_to_queue(video2)
+    add_video_to_queue(video3)
+
+    set_video_position(video1,3)
+    set_video_position(video2,1)
+    set_video_position(video3,2)
+    update_queue
+
+    expect_video_position(video1, 3)
+    expect_video_position(video2, 1)
+    expect_video_position(video3, 2)
+  end
+
+  def expect_video_to_be_in_queue(video)
+    expect(page).to have_content(video.title)
+  end
+
+  def expect_link_not_to_be_seen(link_text)
+    expect(page).not_to have_content(link_text)
+  end
+
+  def update_queue
+    click_button("Update Instant Queue")
+  end
+
+  def add_video_to_queue(video)
     visit home_path
-    find("a[href='#{video_path(video2)}']").click
+    find("a[href='#{video_path(video)}']").click
     click_link("+ My Queue")
-    visit home_path
-    find("a[href='#{video_path(video3)}']").click
-    click_link("+ My Queue")
+  end
 
+  def set_video_position(video, position)
+    within(:xpath, "//tr[contains(.,'#{video.title}')]") do
+      fill_in "queue_items[][position]", with: position
+    end
+  end
 
+  def expect_video_position(video, position)
+    expect(find(:xpath, "//tr[contains(.,'#{video.title}')]//input[@type='text']").value).to eq(position.to_s)
   end
 end
