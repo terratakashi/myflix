41ee6ab6fcabf44baee2110b9a03801bb86b40e7
terratakashi <terratakashi@gmail.com>
2013-11-04 00:27:17 +0800

    changed macro name to clear_current_user

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,6 +1,6 @@
 shared_examples "requires sign in" do
   it "redirects to the sign in page" do
-    user_sign_out
+    clear_current_user
     action
     expect(response).to redirect_to sign_in_path
   end
--- spec/support/user_macros.rb
+++ spec/support/user_macros.rb
@@ -6,6 +6,6 @@ def current_user
   @user ||= User.find(session[:user_id])
 end
 
-def user_sign_out
+def clear_current_user
   session[:user_id] = nil
 end
