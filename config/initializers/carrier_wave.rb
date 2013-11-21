7d1365af0193a4467d5584acb5063779f9a5e360
terratakashi <terratakashi@gmail.com>
2013-11-21 22:17:15 +0800

    fixed bugs for S3 config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
-  if Rails.env_staging? || Rails.env.production?
+  if Rails.env.staging? || Rails.env.production?
     config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => 'ENV['AWS_ACCESS_KEY_ID']',
-      :aws_secret_access_key  => 'ENV['AWS_SECRET_ACCESS_KEY']',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
     }
     config.fog_directory  = 'terratakashi-myflix'
   else
