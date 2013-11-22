3e9e05adaab9b06e36d92d900ec9c981d588eca2
terratakashi <terratakashi@gmail.com>
2013-11-22 15:35:35 +0800

    added a video_url to the view, controller and table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131119074703) do
+ActiveRecord::Schema.define(version: 20131121142654) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20131119074703) do
     t.string   "large_cover"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "video_url"
   end
 
 end
