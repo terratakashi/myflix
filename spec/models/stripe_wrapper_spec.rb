c5a4c4eb66a7ebaab79860f49ee62162a32e9faa
terratakashi <terratakashi@gmail.com>
2013-11-29 17:09:48 +0800

    refactor stripe wrapper spec

--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -2,20 +2,23 @@ require "spec_helper"
 
 describe StripeWrapper do
   describe StripeWrapper::Charge, :vcr do
-    before { StripeWrapper.set_api_key }
+    let(:charge) do
+      StripeWrapper.set_api_key
+      token = Stripe::Token.create(
+                :card => {
+                  :number => card_number,
+                  :exp_month => 11,
+                  :exp_year => 2016,
+                  :cvc => "123"} ).id
+      StripeWrapper::Charge.create(
+        :token => token,
+        :email => "test_mode@example.com")
+    end
 
     context "with valid card" do
+      let(:card_number) {"4242424242424242"}
+
       it "charges the card successfully" do
-        card_number = "4242424242424242"
-        token = Stripe::Token.create(
-                  :card => {
-                    :number => card_number,
-                    :exp_month => 11,
-                    :exp_year => 2016,
-                    :cvc => "123"} ).id
-        charge = StripeWrapper::Charge.create(
-          :token => token,
-          :email => "test_mode@example.com")
         expect(charge).to be_successful
         expect(charge.response.amount).to eq(999)
         expect(charge.response.currency).to eq("usd")
@@ -23,35 +26,13 @@ describe StripeWrapper do
     end
 
     context "with invalid card" do
+      let(:card_number) {"4000000000000002"}
+
       it "doesn't charge the card successfully" do
-        card_number = "4000000000000002"
-
-        token = Stripe::Token.create(
-                  :card => {
-                    :number => card_number,
-                    :exp_month => 11,
-                    :exp_year => 2016,
-                    :cvc => "123"} ).id
-
-        charge = StripeWrapper::Charge.create(
-          :token => token,
-          :email => "test_mode@example.com")
-
         expect(charge).not_to be_successful
       end
 
       it "set a error message" do
-        card_number = "4000000000000002"
-        token = Stripe::Token.create(
-                  :card => {
-                    :number => card_number,
-                    :exp_month => 11,
-                    :exp_year => 2016,
-                    :cvc => "123"} ).id
-        charge = StripeWrapper::Charge.create(
-          :token => token,
-          :email => "test_mode@example.com")
-
         expect(charge.error_message).to be_present
       end
     end
